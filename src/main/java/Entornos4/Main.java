785c44762a73b4f68199f8331c4316cc21b52dee
Laura Moreno Díaz <lauramd022@gmail.com>
2023-02-20 20:38:21 +0100

    Añadimos el codigo

--- src/main/java/Entornos4/Main.java
+++ src/main/java/Entornos4/Main.java
@@ -14,27 +14,34 @@ public class Main {
 
         try {
 
-            NuevoMetodo(cesta, 50, 100);
+            NuevoMetodo(cesta, 200, 100);
 
         } catch (Exception e) {
 
             System.out.println("No se puede pagar  ");
 
         }
-        
+
         cesta.setBono(100);
 
         System.out.println("Su bono actual es  " + cesta.getBono());
 
+        try {
+            System.out.println(" Modificación 1");
+            cesta.setBono(-50);
+        } catch (Exception e) {
+            System.out.print("Fallo……");
+
+        }
+
     }
 
     public static void NuevoMetodo(eCesta cesta, double cantidad, double recargarCantidad) throws Exception {
-        
+
         cesta.comprar(cantidad);
-        
+
         System.out.println("Solicitar actualización de bono");
         cesta.setBono(recargarCantidad);
-        
-    }
 
+    }
 }
